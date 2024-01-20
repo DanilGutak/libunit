054f7e45ab47596cd553a63b341eb1e0eecd22cf
DanilGutak <otecgutak@gmail.com>
2024-01-20 15:25:10 +0100

    checking

--- tests/strlen/02_null_test.c
+++ tests/strlen/02_null_test.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   00_strlen_launcher.c                               :+:      :+:    :+:   */
+/*   02_null_test.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dgutak <dgutak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 12:35:07 by aapenko           #+#    #+#             */
-/*   Updated: 2024/01/20 14:53:34 by dgutak           ###   ########.fr       */
+/*   Updated: 2024/01/20 14:53:10 by dgutak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,8 @@
 
 int basic_test(void)
 {
-	if (ft_strlen("Hello") == 6)
+	if (ft_strlen("") == 6)
 		return (0);
 	else
 		return (-1);
-}
-
-int big_test(void)
-{
-	if (ft_strlen("Hello, I know you, you are an idiot!") == 36)
-		return (0);
-	else
-		return (-1);
-}
-
-
-int strlen_launcher(void)
-{
-	t_unit_test *testlist;
-
-	load_test(&testlist, "Basic test", &basic_test);
-	load_test(&testlist, "NULL test", &null_test);
-	load_test(&testlist, "Big test", &big_test);
-	return (launch_tests(&testlist));
 }
--- tests/strlen/04_empty_test.c
+++ tests/strlen/04_empty_test.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   00_strlen_launcher.c                               :+:      :+:    :+:   */
+/*   04_empty_test.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dgutak <dgutak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 12:35:07 by aapenko           #+#    #+#             */
-/*   Updated: 2024/01/20 14:53:34 by dgutak           ###   ########.fr       */
+/*   Updated: 2024/01/20 14:52:05 by dgutak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,4 @@ int basic_test(void)
 		return (0);
 	else
 		return (-1);
-}
-
-int big_test(void)
-{
-	if (ft_strlen("Hello, I know you, you are an idiot!") == 36)
-		return (0);
-	else
-		return (-1);
-}
-
-
-int strlen_launcher(void)
-{
-	t_unit_test *testlist;
-
-	load_test(&testlist, "Basic test", &basic_test);
-	load_test(&testlist, "NULL test", &null_test);
-	load_test(&testlist, "Big test", &big_test);
-	return (launch_tests(&testlist));
 }
